e92c71ad3318f38c427cf6468f72cd4833c311ac
U-ROBOT\Abhishek <abhicoders@gmail.com>
2016-01-21 01:03:09 +0530

    Extreme Gradient Boosting Model with features prepared from log file

--- scripts/models.py
+++ scripts/models.py
@@ -4,6 +4,8 @@ from sklearn.preprocessing import StandardScaler
 
 from sklearn.ensemble import RandomForestClassifier
 
+import xgboost as xgb
+
 from features import FeatureTransformer
 
 def build_logistic_regression_model(X, X_test):
@@ -18,7 +20,7 @@ def build_logistic_regression_model(X, X_test):
 
 def build_random_forest_classifier(X, X_test):
 	ft = FeatureTransformer(X, X_test)
-	clf = RandomForestClassifier(n_estimators=100)
+	clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
 
 	pipeline = Pipeline([('ft', ft), ('clf', clf)])
 
@@ -33,3 +35,11 @@ def build_sgd_classifier(X, X_test):
 	pipeline = Pipeline([('ft', ft), ('scaler', scaler), ('clf', clf)])
 
 	return Pipeline
+
+def build_extreme_gradient_boosting(X, X_test):
+	ft = FeatureTransformer(X, X_test)
+	clf = xgb.XGBClassifier()
+
+	pipeline = Pipeline([('ft', ft), ('clf', clf)])
+
+	return pipeline
